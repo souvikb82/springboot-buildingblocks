f0ef751ab56017b3590107eb8b4761b357a266fa
souvikb82 <souvik0682@gmail.com>
2023-09-25 13:08:12 -0500

    2nd commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
+	//Starting point of the sb app
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
